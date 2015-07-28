1678e0561e76be5e0cd45ae7668ebecf1a2a5b42
Akhil Narang <akhilnarang.1999@gmail.com>
2015-07-28 12:27:56 +0000

    :D Signed-off-by: RadiumBot <teamradium2015@gmail.com>
    Change-Id: Iaf1f38c96bd61e3cab3d31ab1918ab93452bfdec

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,8 +20,12 @@
 # Kernel
 TARGET_KERNEL_CONFIG := ownbacon_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
-LOCALVERSION := Radium
-KERNEL_TOOLCHAIN := /home/akhilnarang/radium/prebuilts/gcc/linux-x86/arm/arm-eabi-6.0/bin
+
+ifeq ($(RADIUMBOT),true)
+    KERNEL_TOOLCHAIN := /root/radium/prebuilts/gcc/linux-x86/arm/arm-eabi-6.0/bin
+else
+    KERNEL_TOOLCHAIN := /home/akhilnarang/radium/prebuilts/gcc/linux-x86/arm/arm-eabi-6.0/bin
+endif
 
 # Optimizations
 ROM_OPTIS := true
